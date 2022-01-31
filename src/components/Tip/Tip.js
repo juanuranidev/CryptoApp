6852af0a1f4ed3d07846b93bfe3b4d3a3a05fc44
Juan Urani <juanuranidev@gmail.com>
2022-01-30 22:18:59 -0300

    updates

--- src/components/Tip/Tip.js
+++ src/components/Tip/Tip.js
@@ -5,7 +5,7 @@ const Tip = () => {
   return (
     <div className="tip">
         <span className="fab fa-bitcoin fa-3x refreshCoins tip_span" />
-        <p className="tip_p">Click on the image of a cryptocurrency to go to his page</p>
+        <p className="tip_p">Click on the image of a cryptocurrency to go to its page</p>
     </div>
     );
 };
